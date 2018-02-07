762650e94272bca83ae88ba1e9496ec35ee29130
KirillRoman0v <romanoffkir@yandex.ru>
2018-02-08 01:28:29 +0300

    last commit

--- labs/lab1/KirillRoman0v/lab1.c
+++ labs/lab1/KirillRoman0v/lab1.c
@@ -2,11 +2,10 @@
 
 union X {
   float fl;
-  unsigned char binary[4];
   int integ;
 };
 
-void FloatToBin(char &bin[33], float f) {
+char FloatToBin(char bin[33], float f) {
   float result;
   union X x;
   int buf, n;
@@ -24,25 +23,53 @@ void FloatToBin(char &bin[33], float f) {
 
   }
   bin[32]='\0';
+  return *bin;
 }
 
-float BinToFloat (float f, unsigned char bin[4]) {
+float BinToFloat (int buf) {
   union X x;
-  int i;
-  for (i=0; i<4; i++)
-    x.binary[i]=bin[i];
+  float f;
+  x.integ = buf;
   f = x.fl;
   return f;
 }
 
 int main () {
   union X x;
+  int menu, buf;
   char bin[33];
-  //printf ("Select 1 or 2:\n");
-  //printf ("\t1)from Binary to Float \n");
-  //printf ("\t2)from Float to Binary \n");
-
-
-
+  float result, bufFloat;
+  int i;
+  x.integ = 0;
+  printf ("Select 1 or 2:\n");
+  printf ("\t1)from Binary to Float \n");
+  printf ("\t2)from Float to Binary \n");
+  scanf("%d", &menu);
+  switch (menu) {
+    case 1: {
+      printf ("Write a 32 bytes:  ");
+      for (i=0; i<32; i++) {
+          scanf ("%d", &buf);
+          x.integ=x.integ << 1;
+          if (buf==1)
+            x.integ+=1;
+      }
+      printf("\n");
+      result = BinToFloat(x.integ);
+      printf ("INT %d\n", x.integ);
+      printf("RESULT: %f\n", result);
+      break;
+    }
+    case 2: {
+      printf ("Write a number with a floating point: ");
+      scanf ("%f", &bufFloat);
+      printf ("\n");
+      *bin = FloatToBin (bin, bufFloat);
+      printf ("RESULT: %s\n", bin);
+      break;
+    }
+    default:
+      break;
+  }
   return 0;
 }
