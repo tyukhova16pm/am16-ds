59b537229f9e6256cc1598edecb030c1a6485579
Максим Смирнов <smirnovmaksim10@mail.ru>
2018-03-16 14:56:25 +0300

    Update list.h

--- labs/lab5/NeoZet/list.h
+++ labs/lab5/NeoZet/list.h
@@ -10,11 +10,3 @@ Node* initList(int key, char* value);
 void addNode(Node** head, int key, char* value);
 Node* findNode(Node* head, int key);
 void delNode(Node** head, int key);
-// typedef struct {
-// 	int size;
-// 	Node *head;
-// } List;
-
-// List* initList(int key, char* value);
-// void addNode(List *list, int key, char* value);
-// int delNode(List *list, int key);
