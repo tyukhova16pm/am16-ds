716eacacf13dc19691668e41ba87bf97b4593bf3
Аля <alla.gorenkova@gmail.com>
2018-03-24 00:48:45 +0300

    Rename labs/lab4/Rudeigin/map_of_table.cpp to labs/lab5/Rudeigin/map_of_table.cpp

--- labs/lab5/Rudeigin/map_of_table.cpp
+++ labs/lab5/Rudeigin/map_of_table.cpp
@@ -5,25 +5,26 @@
 #include "map_of_table.h"
 
 map_of_table::map_of_table(){
-    table = new table [20];
+    _table = new table [20];
     size = 20;
 }
 
 map_of_table::map_of_table(int _size) {
-    table = new table(_size);
+    _table = new table(_size);
     size = _size;
 }
 
 map_of_table::~map_of_table() {
-    delete table;
+    delete _table;
     size = 0;
 }
 
 void map_of_table::set(int key, Man man) {
     std::string value = man.telephone + " " + man.surname + " " + man.name;
-    table->set(key, value);
+    _table->set(key, value);
 }
 
 std::string map_of_table::get(int key) {
-    return table->get(key);
+    return _table->get(key);
 }
+
