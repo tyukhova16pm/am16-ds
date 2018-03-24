b6f1adc3f3f698d6e697c6223a7a5348b532404b
Аля <alla.gorenkova@gmail.com>
2018-03-24 05:02:26 +0300

    fix

--- labs/lab4/Rudeigin/tree.h
+++ labs/lab4/Rudeigin/tree.h
@@ -4,25 +4,30 @@
 
 class huffman_tree {
 private:
-    tree * left;
-    tree * right;
+    huffman_tree * left;
+    huffman_tree * right;
     char symbol;
     int weight;
 public:
-    huffman_tree(tree *l = NULL, tree *r = NULL, char sym = 0, int w = 0);
+    huffman_tree(huffman_tree *l = NULL, huffman_tree *r = NULL, char sym = 0, int w = 0);
     ~huffman_tree();
     bool is_leaf();
+
+    huffman_tree * get_left();
+    huffman_tree * get_right();
+    int get_weight();
+    char get_symbol();
 };
 
 union bytec {
     unsigned int number;
     unsigned char bytes[4];
 };
-#endif // TREE
 
-huffman_tree * make_node(tree * left_child, tree * right_child);
-int min_id(tree ** trees);
-tree * make_huffman_tree(tree **forest);
-void compress_way(tree * _tree, string *codes, string current_code);
-tree * decode(tree * big_tree, string str);
-void uncompress_way(tree *_tree, string input, string output, string * codes);
+huffman_tree * make_node(huffman_tree * left_child, huffman_tree * right_child);
+int min_id(huffman_tree ** trees);
+huffman_tree * make_huffman_tree(huffman_tree **forest);
+void way(huffman_tree *_tree, std::string *codes, std::string current_code);
+huffman_tree * decode(huffman_tree * big_tree, std::string str);
+void uncompress(huffman_tree *_tree, std::string input, std::string output, std::string * codes);
+#endif // TREE
