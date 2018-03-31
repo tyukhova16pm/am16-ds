51643d67ffaf72500dcda0836116bc070133ffd6
Аля <alla.gorenkova@gmail.com>
2018-03-31 13:33:21 +0300

    fix class

--- labs/lab7/Rudeigin/cat.cpp
+++ labs/lab7/Rudeigin/cat.cpp
@@ -22,3 +22,23 @@ std::string cat::get_breed() {
 std::string cat::get_food() {
     return food;
 }
+
+void cat::set_id(int _id) {
+    id = _id;
+}
+
+void cat::set_age(int _age) {
+    age = _age;
+}
+
+void cat::set_name(std::string _name) {
+    name = _name;
+}
+
+void cat::set_breed(std::string _breed) {
+    breed = _breed;
+}
+
+void cat::set_food(std::string _food) {
+    food = _food;
+}
